43ac17bc61034db7098ffcb3d635f05702c3e5b1
Razevich <razevich.c@gmail.com>
2016-01-31 11:58:21 -0600

    add pikiwik gem

--- crumbs/db/migrate/20160127030059_create_students.rb
+++ crumbs/db/migrate/20160127030059_create_students.rb
@@ -1,6 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+      t.text     :first_name, null: false
+      t.text     :last_name, null: false
+      t.text     :password, null: false
+      t.text     :location
 
       t.timestamps null: false
     end
--- crumbs/db/migrate/20160127030246_create_students_non_profits.rb
+++ crumbs/db/migrate/20160127030246_create_students_non_profits.rb
@@ -1,6 +1,8 @@
 class CreateStudentsNonProfits < ActiveRecord::Migration
   def change
     create_table :students_non_profits do |t|
+      t.integer  :student_id
+      t.integer  :non_profit_id
 
       t.timestamps null: false
     end
