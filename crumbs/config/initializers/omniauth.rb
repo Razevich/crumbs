b97c0fd34403728b5146ef2426950f5bbb2b8dc1
Razevich <razevich.c@gmail.com>
2016-02-02 20:31:27 -0600

    did not post token and secret to github

--- crumbs/config/initializers/omniauth.rb
+++ crumbs/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1533513480282656', 'ce484e6da69ffa655cbab0f9eca45a3b'
+  provider :facebook, 'Token', 'Secret'
 end
