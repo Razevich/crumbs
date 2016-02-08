f89e9db711e660d36e4965a43be24ef05feef63e
Razevich <razevich.c@gmail.com>
2016-02-08 14:55:41 -0600

    make form to register non profits

---
+++ crumbs/app/controllers/non_profits_controller.rb
@@ -0,0 +1,26 @@
+class NonProfitsController < ApplicationController
+
+  def new
+    @non_profit = NonProfit.new
+  end
+
+  def create
+    @non_profit = NonProfit.new(non_profit_params)
+    if @non_profit.save
+      redirect_to non_profit_path(@non_profit)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @non_profit = NonProfit.find_by(id: params[:id])
+  end
+
+  private
+
+  def non_profit_params
+    params.require(:non_profit).permit(:name, :description, :location, :website, :email, :phone)
+  end
+
+end
